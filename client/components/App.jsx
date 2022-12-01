8976e2cbd26a60ad6a80a2e2d4d5c14aa317a746
aliyah <aliyahgwinter@gmail.com>
2022-12-01 16:11:27 +1300

    Complete MVP

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { Suspense, useRef } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Nav from './Nav'
-import Fruits from './Fruits'
-import Profile from './Profile'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { OrbitControls, Stars } from '@react-three/drei'
+import { Physics } from '@react-three/cannon'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import EarthMap from '../../server/public/RS3_EarthC.webp'
+
+function Sphere() {
+  const colorMap = useLoader(TextureLoader, EarthMap)
+  const earthRef = useRef()
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime()
+    earthRef.current.rotation.y = elapsedTime / 6
+  })
+  return (
+    <mesh ref={earthRef}>
+      <sphereGeometry />
+      <meshStandardMaterial map={colorMap} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <>
-      <Nav />
-      <main className="flex items-center justify-center h-screen">
-        <Routes>
-          <Route path="/" element={<Fruits />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </main>
-    </>
+    <Canvas>
+      <Suspense>
+        <OrbitControls />
+        <Stars count={20000} fade={true} />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Physics>
+          <Sphere />
+        </Physics>
+      </Suspense>
+    </Canvas>
   )
 }
 
