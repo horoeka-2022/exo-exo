54934176f8f2f6b319833d1f09dcfe0e1a75a020
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-03 12:14:50 +1300

    click to zoom working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useRef } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from '@react-three/drei'
+import { Bounds, OrbitControls, Stars, useBounds } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import Earth from './Earth'
 import HD80606b from './HD80606b'
@@ -13,22 +13,65 @@ import Cancri from './Cancri'
 
 function App() {
   return (
-    <Canvas>
-      <Suspense>
-        <OrbitControls enablePan={false} />
-        <Stars count={20000} fade={true} />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Physics>
-          <Cancri position={[0, 25, 0]} args={[1.875, 32, 32]} />
-          <Earth position={[0, 0, 0]} />
-          <HD80606b position={[0, 5, 0]} args={[0.3, 32, 32]} />
-          <HD189733b position={[0, -15, 0]} args={[7, 32, 32]} />
-          <Kepler22B position={[0, 13, 0]} args={[2.4, 32, 32]} />
-          <Upsilon position={[0, -45, 0]} args={[15, 32, 32]} />
-        </Physics>
+    <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]}>
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <OrbitControls
+        enablePan={false}
+        makeDefault
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI / 1.75}
+      />
+      <Stars count={20000} fade={true} />
+      <ambientLight intensity={0.5} />
+      <Suspense fallback={null}>
+        <Bounds fit clip observe margin={1.2}>
+          <SelectToZoom>
+            <Physics>
+              <Cancri
+                position={[-20, -11, -20]}
+                args={[1.875, 32, 32]}
+                rotation={[4, 0, -0]}
+              />
+              <Earth position={[0, 0, 0]} rotation={[1, 1, -2]} />
+              <HD80606b
+                position={[0, 5, 0]}
+                args={[0.3, 32, 32]}
+                rotation={[1, 0, -1]}
+              />
+              <HD189733b
+                position={[0, -15, 0]}
+                args={[7, 32, 32]}
+                rotation={[2, 0, 1]}
+              />
+              <Kepler22B
+                position={[0, 13, 0]}
+                args={[2.4, 32, 32]}
+                rotation={[1, 1, 0]}
+              />
+              <Upsilon
+                position={[0, -45, 0]}
+                args={[15, 32, 32]}
+                rotation={[1, 0, 0]}
+              />
+            </Physics>
+          </SelectToZoom>
+        </Bounds>
       </Suspense>
     </Canvas>
+  )
+}
+
+function SelectToZoom({ children }) {
+  const api = useBounds()
+  return (
+    <group
+      onClick={(e) => (
+        e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit()
+      )}
+      onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}
+    >
+      {children}
+    </group>
   )
 }
 
