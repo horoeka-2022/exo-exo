cf5f0913f1a1945f32b89441b04a9d69b491f501
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-04 09:53:04 +1300

    working on onclick functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,14 @@ import React, { Suspense, useRef } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { Canvas } from '@react-three/fiber'
-import { Bounds, OrbitControls, Stars, useBounds } from '@react-three/drei'
+import {
+  Bounds,
+  ContactShadows,
+  Loader,
+  OrbitControls,
+  Stars,
+  useBounds,
+} from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import Earth from './Earth'
 import HD80606b from './HD80606b'
@@ -13,51 +20,64 @@ import Cancri from './Cancri'
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]}>
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <OrbitControls
-        enablePan={false}
-        makeDefault
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI / 1.75}
-      />
-      <Stars count={20000} fade={true} />
-      <ambientLight intensity={0.5} />
-      <Suspense fallback={null}>
-        <Bounds fit clip observe margin={1.2}>
-          <SelectToZoom>
-            <Physics>
-              <Cancri
-                position={[-20, -11, -20]}
-                args={[1.875, 32, 32]}
-                rotation={[4, 0, -0]}
-              />
-              <Earth position={[0, 0, 0]} rotation={[1, 1, -2]} />
-              <HD80606b
-                position={[0, 5, 0]}
-                args={[0.3, 32, 32]}
-                rotation={[1, 0, -1]}
-              />
-              <HD189733b
-                position={[0, -15, 0]}
-                args={[7, 32, 32]}
-                rotation={[2, 0, 1]}
-              />
-              <Kepler22B
-                position={[0, 13, 0]}
-                args={[2.4, 32, 32]}
-                rotation={[1, 1, 0]}
-              />
-              <Upsilon
-                position={[0, -45, 0]}
-                args={[15, 32, 32]}
-                rotation={[1, 0, 0]}
-              />
-            </Physics>
-          </SelectToZoom>
-        </Bounds>
-      </Suspense>
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [0, -10, 80], fov: 50 }} dpr={[1, 2]}>
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <OrbitControls
+          enablePan={false}
+          makeDefault
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 1.75}
+        />
+        <Stars count={20000} fade={true} />
+        <ambientLight intensity={0.5} />
+        <Suspense fallback={null}>
+          <Bounds fit clip observe margin={1.2}>
+            <SelectToZoom>
+              <Physics>
+                <Cancri
+                  position={[1, 20, -10]}
+                  args={[1.875, 32, 32]}
+                  rotation={[4, 0, -0]}
+                />
+                <Earth position={[0, 0, 0]} rotation={[1, 1, -2]} />
+                <HD80606b
+                  position={[20, 1, -10]}
+                  args={[0.3, 32, 32]}
+                  rotation={[1, 0, -1]}
+                />
+                <HD189733b
+                  position={[-20, 5, -1]}
+                  args={[7, 32, 32]}
+                  rotation={[2, 0, 1]}
+            
+                />
+                <Kepler22B
+                  position={[18, 15, -25]}
+                  args={[2.4, 32, 32]}
+                  rotation={[1, 1, 0]}
+                />
+                <Upsilon
+                  position={[0, -40, -50]}
+                  args={[15, 32, 32]}
+                  rotation={[1, 0, 0]}
+                />
+              </Physics>
+            </SelectToZoom>
+          </Bounds>
+          <ContactShadows
+            rotateX={Math.PI / 2}
+            position={[0, -35, 0]}
+            opacity={0.2}
+            width={200}
+            height={200}
+            blur={1}
+            far={50}
+          />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   )
 }
 
