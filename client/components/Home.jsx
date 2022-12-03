219c8d983cb7e634f1921e78f1ba9aae36606c1f
daph <daphnejasminesimons@gmail.com>
2022-12-03 15:06:55 +1300

    attempt at making 3dtext reflective, thanks lots of love, daph

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useLoader, extend } from '@react-three/fiber'
-import { Html } from '@react-three/drei'
+import { Html, useKeyboardControls } from '@react-three/drei'
 import React, { useRef } from 'react'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import PlaneTexture from '../../server/public/glass1.jpg'
@@ -8,6 +8,7 @@ import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 import myFont from '../../server/public/fonts/Trispace.json'
 import PlanetText from '../../server/public/HD80606b.jpg'
+import { FrontSide, DoubleSide } from 'three'
 
 extend({ TextGeometry })
 
@@ -19,11 +20,19 @@ export default function Home() {
 
   return (
     <>
+      {/* plane screen */}
       <mesh ref={planeRef} position={[0, 0, 2]}>
         <ambientLight intensity={0.2} />
         <spotLight position={[0, 0, 5]} angle={10} intensity={1} />
-        <meshStandardMaterial map={planeMap} opacity={0.6} transparent />
+        <meshStandardMaterial
+          map={planeMap}
+          opacity={0.6}
+          transparent
+          side={FrontSide}
+          reflectivity={10}
+        />
         <planeGeometry args={[12, 6]} />
+        {/* Html incorporation */}
         <Html>
           <div>
             <center>{/* <h1>Hello World!</h1> */}</center>
@@ -31,17 +40,19 @@ export default function Home() {
         </Html>
       </mesh>
 
-      {/* text */}
+      {/* title */}
       <mesh position={[-2.5, 0, 4]}>
         <ambientLight intensity={1} />
         <spotLight position={[0, 0, 5]} angle={1} intensity={1} />
         <textGeometry args={['EXO EXO', { font, size: 0.8, height: 0.5 }]} />
         <meshLambertMaterial attach="material" color={'silver'} />
         <meshStandardMaterial
-          metallic={10}
+          metalness={3}
+          roughness={0.1}
           map={textMap}
-          opacity={0.7}
+          opacity={1}
           transparent
+          reflectivity={0}
         />
       </mesh>
     </>
