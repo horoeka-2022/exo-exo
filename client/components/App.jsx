ad558ceb50a4adba0a26b251233d7b747565338d
Rav Ariyanayagam <thisisravnow@gmail.com>
2022-12-03 19:32:42 +1300

    Add orbit component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,15 @@
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Orbit from './Orbit'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars, Bounds, useBounds } from '@react-three/drei'
+import {
+  OrbitControls,
+  Stars,
+  Bounds,
+  useBounds,
+  Html,
+} from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import Earth from './Earth'
 import HD80606b from './HD80606b'
@@ -26,7 +33,9 @@ import Planet12 from './Planet12'
 
 function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 0], fov: 30 }} dpr={[1, 2]}>
+    // <Canvas camera={{ position: [0, 0, 0], fov: 30 }} dpr={[1, 2]}>
+    <Canvas camera={{ position: [0, 20, 25], fov: 45 }} dpr={[1, 2]}>
+      <Orbit />
       <spotLight position={[10, 15, 10]} angle={0.3} />
       <OrbitControls
         enablePan={false}
@@ -34,14 +43,21 @@ function App() {
         minPolarAngle={0}
         maxPolarAngle={Math.PI / 1.75}
       />
-      <Stars count={50000} fade={true} />
-      <ambientLight intensity={0.5} />
+      <Stars
+        radius={500} // Radius of the inner sphere (default=100)
+        depth={30} // Depth of area where stars should fit (default=50)
+        count={10000} // Amount of stars (default=5000)
+        factor={5} // Size factor (default=4)
+        saturation={0.7} // Saturation 0-1 (default=0)
+        fade
+      />
+      <ambientLight intensity={0.8} />
       <Suspense fallback={null}>
         <Bounds fit clip observe margin={1.2}>
           <SelectToZoom>
             <Physics>
               <Cancri
-                position={[-20, -11, -20]}
+                position={[-20, -11, -80]}
                 args={[1.875, 32, 32]}
                 rotation={[4, 0, -0]}
               />
@@ -52,31 +68,31 @@ function App() {
                 rotation={[1, 0, -1]}
               />
               <HD189733b
-                position={[0, -15, 0]}
+                position={[0, -15, -80]}
                 args={[7, 32, 32]}
                 rotation={[2, 0, 1]}
               />
               <Kepler22B
-                position={[0, 13, 0]}
-                args={[2.4, 32, 32]}
+                position={[0, -23, 160]}
+                args={[2.4, 82, 32]}
                 rotation={[1, 1, 0]}
               />
               <Upsilon
                 position={[0, -45, 0]}
-                args={[5, 32, 32]}
+                args={[8, 32, 32]}
                 rotation={[1, 0, 0]}
               />
 
-              <Planet1 position={[64, -15, -236]} args={[1.2, 32, 32]} />
-              <Planet2 position={[7, -60, 150]} args={[1.5, 32, 32]} />
-              <Planet3 position={[-37, -15, 108]} args={[1.7, 32, 32]} />
-              <Planet4 position={[7, -125, 28]} args={[1.8, 32, 32]} />
-              <Planet5 position={[-55, -17, -6]} args={[8.1, 32, 32]} />
-              <Planet6 position={[-13, 59, -230]} args={[2.7, 32, 32]} />
+              <Planet1 position={[64, -15, -236]} args={[0.8, 32, 32]} />
+              <Planet2 position={[7, -60, 150]} args={[1.2, 32, 32]} />
+              <Planet3 position={[-37, -85, 108]} args={[1.7, 32, 32]} />
+              <Planet4 position={[7, -125, 328]} args={[6, 32, 32]} />
+              <Planet5 position={[-55, -17, -6]} args={[3.1, 32, 32]} />
+              <Planet6 position={[-13, 59, -230]} args={[2.0, 32, 32]} />
               <Planet7 position={[20, 2, -120]} args={[1.7, 32, 32]} />
               <Planet8 position={[118, 92, -129]} args={[1.9, 32, 32]} />
               <Planet9 position={[-20, 104, 98]} args={[1.4, 32, 32]} />
-              <Planet10 position={[1, -50, -26]} args={[2.8, 32, 32]} />
+              <Planet10 position={[1, -50, -26]} args={[2.1, 32, 32]} />
               <Planet11 position={[-28, -15, 28]} args={[3.7, 32, 32]} />
               <Planet12 position={[-20, 10, 90]} args={[1.8, 32, 32]} />
             </Physics>
