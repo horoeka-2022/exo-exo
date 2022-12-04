5720a3d7380a7156cf71c8f09efcd24d25fbc96b
Aliyah Winter <aliyahgwinter@gmail.com>
2022-12-04 14:41:52 +1300

    Refactor and update styling

--- client/components/Typing.jsx
+++ client/components/Typing.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 
+const cancri = [
+  'Name : 55 Cancri e.',
+  'Description : A molten surface planet with sparkling silicate skies. Approximately 8 times heavier than Earth with twice the diameter.',
+  'Distance From Earth: 40 light years.',
+  'Habitability : No.',
+  'Atmosphere : Carbon, Nitrogen, Oxygen.',
+  'Temperature : 2700 degrees Celsius.',
+  'Size : 1.875 x Earth.',
+  'Year of Discovery: 2004.',
+  'Orbit : 0.7 days.',
+]
+
 function Typing() {
-  return (
-    <div className="planet-info">
-      <Typewriter
-        words={['Name : 55 Cancri e']}
-        cursor
-        cursorStyle="_"
-        cursorBlinking={false}
-        typeSpeed={50}
-      />
-      <Typewriter
-        words={[
-          'Description: A molten surface planet with sparkling silicate skies. Approximately 8 times heavier than Earth with twice the diameter',
-        ]}
-        cursor
-        cursorStyle="_"
-        cursorBlinking={false}
-        typeSpeed={50}
-      />
-      <Typewriter
-        words={['Distance from Earth: 40 light years']}
-        cursor
-        cursorStyle="_"
-        cursorBlinking={false}
-        typeSpeed={50}
-      />
-    </div>
-  )
+  return cancri.map((line) => {
+    return (
+      <div className="planet-info">
+        <Typewriter words={[line]} typeSpeed={20} className="info-line" />
+      </div>
+    )
+  })
 }
 
 export default Typing
