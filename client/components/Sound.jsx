ead15f9ba33ada46087bac3b9b63d9f3463ee218
daph <daphnejasminesimons@gmail.com>
2022-12-04 18:12:43 +1300

    all good now

--- client/components/Sound.jsx
+++ client/components/Sound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sound from 'react-sound'
-import SpaceMusic from '../../server/public/SpaceMusic.mp3'
+import SpaceMusic from '../../server/public/Spacemusic.mp3'
 
 export default function Music() {
   return <Sound url={SpaceMusic} playStatus={Sound.status.PLAYING} />
