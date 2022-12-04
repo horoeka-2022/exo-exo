7c4f1613f34230e9b243e18517265cf748d8adaf
daph <daphnejasminesimons@gmail.com>
2022-12-04 18:05:02 +1300

    fix allgood, thanks everyone, lots of love, daph

--- client/components/planets/Upsilon.jsx
+++ client/components/planets/Upsilon.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
-import UpsilonMap from '../../../server/public/Upsilon.jpeg'
+import UpsilonMap from '../../../server/public/textures/Upsilon.jpeg'
 import { useFrame, useLoader } from '@react-three/fiber'
 
 export default function Upsilon({ position, args }) {
