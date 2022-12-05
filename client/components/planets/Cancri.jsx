0622a9e0ecf382ed2076dde28ef0ad553467c227
Aliyah Winter <aliyahgwinter@gmail.com>
2022-12-06 10:05:07 +1300

    experiment with glowing

--- client/components/planets/Cancri.jsx
+++ client/components/planets/Cancri.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import CancriMap from '../../../server/public/textures/2k_sun.jpeg'
 import { useFrame, useLoader } from '@react-three/fiber'
-import { Sparkles } from '@react-three/drei'
+import { EffectComposer, Bloom, GodRays } from '@react-three/postprocessing'
 
 export default function Cancri({ position, args }) {
   const textureMap = useLoader(TextureLoader, CancriMap)
@@ -13,16 +13,29 @@ export default function Cancri({ position, args }) {
     cancriRef.current.rotation.y = elapsedTime / 6
   })
   return (
-    <mesh ref={cancriRef} position={position}>
-      <sphereGeometry args={args} />
-      <meshStandardMaterial map={textureMap} scale={5} />
-      <Sparkles
-        count={50}
-        scale={5}
-        size={2}
-        speed={0}
-        noise={[0.1, 0.1, 0.1]}
-      />
-    </mesh>
+    <>
+      <mesh ref={cancriRef} position={position}>
+        <sphereGeometry args={args} />
+        <meshStandardMaterial map={textureMap} scale={5} />
+      </mesh>
+        {/* <GodRays
+          cancri={cancriRef.current}
+          samples={30}
+          density={0.97}
+          decay={0.96}
+          weight={0.6}
+          exposure={0.4}
+          clampMax={1}
+          blur={true}
+        /> */}
+        <EffectComposer>
+          <Bloom
+            luminanceThreshold={0.5}
+            luminanceSmoothing={0.9}
+            height={300}
+            opacity={3}
+          />
+      </EffectComposer>
+    </>
   )
 }
