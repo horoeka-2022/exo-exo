85c73206d75191c6e58f39f9676e26f1ca18db1b
shymala <shymala.sidharth22@gmail.com>
2022-12-07 10:14:54 +1300

    updated typing effect

--- client/components/Typing.jsx
+++ client/components/Typing.jsx
@@ -3,9 +3,9 @@ import { Typewriter } from 'react-simple-typewriter'
 
 function Typing({ line, typeSpeed }) {
   return (
-    <div className="planet-info">
+    
       <Typewriter words={[line]} typeSpeed={typeSpeed} className="info-line" />
-    </div>
+    
   )
 }
 
