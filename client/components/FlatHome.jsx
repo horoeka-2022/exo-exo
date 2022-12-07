46b20c22a1ffbe0681fae78ba5c5ca0c8e9b58b8
Daphne Simons <daphnejasminesimons@gmail.com>
2022-12-07 15:06:32 +1300

    home page looks much better

--- client/components/FlatHome.jsx
+++ client/components/FlatHome.jsx
@@ -7,19 +7,20 @@ export default function FlatHome() {
   function handleClick() {}
   return (
     <>
-
       <div className="home-body">
         <div className="title-center">
           <h1 className="home-title">exo exo</h1>
         </div>
         <div className="centering">
           <p className="home-type">
-            <Typing
-              line={
-                'An exoplanet is any planet beyond our solar system. Most orbit other stars, but free-floating exoplanets, called rogue planets, orbit the galactic center and are untethered to any star.'
-              }
-              typeSpeed={10}
-            />
+            <div className="home-div">
+              <Typing
+                line={
+                  'An exoplanet is any planet beyond our solar system. Most orbit other stars, but free-floating exoplanets, called rogue planets, orbit the galactic center and are untethered to any star.'
+                }
+                typeSpeed={60}
+              />
+            </div>
           </p>
         </div>
         <div className="button-center">
